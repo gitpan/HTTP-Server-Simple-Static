b1f4b73c45bb3d4833c10d11573130ea90a190de
Stephen Quinney <sjq-perl@jadevine.org.uk>
2006-06-13 11:08:05 +0000

    Import of SJQUINNEY/HTTP-Server-Simple-Static-0.03 from CPAN.
    gitpan-cpan-distribution: HTTP-Server-Simple-Static
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SJQUINNEY/HTTP-Server-Simple-Static-0.03.tar.gz
    gitpan-cpan-author:       SJQUINNEY
    gitpan-cpan-maturity:     released

--- lib/HTTP/Server/Simple/Static.pm
+++ lib/HTTP/Server/Simple/Static.pm
@@ -1,50 +1,62 @@
 package HTTP::Server::Simple::Static;
 use strict;
 use warnings;
-use File::MMagic;
-use MIME::Types;
-use URI::Escape;
-use File::Spec::Functions qw(canonpath);
-require Exporter;
-our $VERSION = '0.02';
-our @ISA = qw(Exporter);
-our @EXPORT = qw(serve_static);
 
-my $mime=MIME::Types->new();
+use File::MMagic ();
+use MIME::Types  ();
+use URI::Escape  ();
+use IO::File     ();
+use File::Spec::Functions qw(canonpath);
+
+require Exporter;
+
+our $VERSION = '0.03';
+our @ISA     = qw(Exporter);
+our @EXPORT  = qw(serve_static);
+
+my $mime  = MIME::Types->new();
 my $magic = File::MMagic->new();
 
-sub serve_static { 
-    my ($self,$cgi,$base) = @_;
+sub serve_static {
+    my ( $self, $cgi, $base ) = @_;
     my $path = $cgi->url( -absolute => 1, -path_info => 1 );
 
     # Sanitize the path and try it.
-    $path = $base.canonpath(uri_unescape($path));
+    $path = $base . canonpath( URI::Escape::uri_unescape($path) );
 
-    if (-e $path and open FILE, $path) {
-	local $/;
-	my $content=<FILE>;
-	close FILE;
-        my $mimeobj= $mime->mimeTypeOf($path);
-        my $mime = ($mimeobj ? $mimeobj->type :
-    			$magic->checktype_contents($content));
-        print "HTTP/1.1 200 OK\n"; 
-        print "Content-type: ", $mime, "\n";
-        print "Content-length: ", length($content), "\n\n";
+    my $fh = IO::File->new();
+    if ( -e $path and $fh->open($path) ) {
+        $fh->binmode;
+        my $content;
+        {
+            local $/;
+            $content = <$fh>;
+        }
+        $fh->close;
+
+        my $mimeobj = $mime->mimeTypeOf($path);
+        my $mime    = ($mimeobj ? $mimeobj->type :
+                       $magic->checktype_contents($content));
+
+        print "HTTP/1.1 200 OK\n";
+        print "Content-type: " . $mime . "\n";
+        print "Content-length: " . length($content) . "\n\n";
         print $content;
-        return 1 ;
-    } 
+        return 1;
+    }
     return 0;
 }
 
 1;
+__END__
 
-=head1 NAME 
+=head1 NAME
 
 HTTP::Server::Simple::Static - Serve static files with HTTP::Server::Simple
 
 =head1 SYNOPSIS
 
-    use base 'HTTP::Server::Simple';
+    use base 'HTTP::Server::Simple::CGI';
     use HTTP::Server::Simple::Static;
 
     sub handle_request {
@@ -61,19 +73,25 @@ subclass.
 =head1 METHODS
 
 =over 4
- 
+
 =item  serve_static
 
 Takes a base directory and a web path, and tries to serve a static file.
 
 =back
 
+=head1 BUGS
+
+Bugs or wishlist requests should be submitted via http://rt.cpan.org/
+
 =head1 SEE ALSO
 
 =head1 AUTHOR
 
-Marcus Ramberg C<marcus@thefeed.no>
-Thanks to Simon Cozens for initial implementation.
+Stephen Quinney C<sjq-perl@jadevine.org.uk>
+
+Thanks to Marcus Ramberg C<marcus@thefeed.no> and Simon Cozens for
+initial implementation.
 
 =head1 LICENSE
 
